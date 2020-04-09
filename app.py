bfac6efb967c3ccdff7701550249056ce3f6c6d3
Roharui <jack555586@gmail.com>
2020-04-09 16:40:56 +0900

    ADD Read CSV

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
 
 from flask import Flask, redirect, url_for, render_template
 from flask import request
+import numpy as np, pandas as pd
 import io
 
 app = Flask(__name__)
+
+allow_file = ['application/vnd.ms-excel']
 
 @app.route('/')
 def index():
@@ -12,7 +15,13 @@ def index():
 @app.route('/dataset', methods=['POST'])
 def dataset():
     file = request.files['file']
+    if file.mimetype in allow_file:
+        readAsCsv(file.read().decode('utf8'))
     return redirect(url_for('index'))
+
+def readAsCsv(file):
+    df = pd.read_csv(io.StringIO(file))
+    print(df.head())
 
 if __name__ == "__main__":              
     app.run(host="0.0.0.0", port="80")
--- static/js/main.js
+++ static/js/main.js
@@ -1,9 +1,22 @@
 
 const fileinput = document.getElementById("fileinput")
+const dataset = Array()
 
 const Send = async () => {
     const files = fileinput.files;
 
+    [...files].map(async (file) => {
+        const fr = new FileReader()
+
+        fr.onloadend = () =>{
+            parseCsvToArray(fr.result)
+            dataSetToTable()
+        }
+
+        fr.readAsText(file, encoding="utf8")
+    })
+
+    /*
     [...files].map(async (img) => {
         const data = new FormData();
         data.append('file', img);
@@ -14,4 +27,53 @@ const Send = async () => {
             body: data
         })
     })
+    */
 }
+
+const parseCsvToArray = (data) => {
+    var allRows = data.split(/\r?\n|\r/);
+    for (var singleRow = 0; singleRow < allRows.length; singleRow++) {
+
+      const arr = Array()
+      var rowCells = allRows[singleRow].split(',');
+      for (var rowCell = 0; rowCell < rowCells.length; rowCell++) {
+          let x = rowCells[rowCell]
+          if(singleRow != 0) {
+              x = parseFloat(x)
+          }
+          arr.push(x)
+      }
+      
+      dataset.push(arr)
+    }
+}
+
+function dataSetToTable() {
+    var table = '<table>';
+    table += '<thead>';
+    table += '<tr>';
+    dataset[0].forEach((x) => {
+        table += '<th>'
+        table += String(x)
+        table += '</th>'
+    })
+    table += '</tr>'
+    //for (var singleRow = 0; singleRow < allRows.length; singleRow++) {
+    dataset.slice(1, 6).forEach(x => {
+        table += '<tr>';
+        x.forEach(y => {
+            table += '<td>'
+            table += String(y)
+            table += '</td>'
+        })
+        table += '</tr>'
+    })
+
+    table += '</tr>';
+    table += '</tbody>';
+    table += '</table>';
+    document.getElementById('table').innerHTML += table;
+}
+
+
+fileinput.addEventListener('change', () => Send())
