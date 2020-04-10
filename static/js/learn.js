c248dc9ec0a2c4097578b7bbaf9007dbe4b1213a
Roharui <jack555586@gmail.com>
2020-04-10 16:56:37 +0900

    ADD divide dataset and model

---
+++ static/js/learn.js
@@ -0,0 +1,40 @@
+
+
+const model_input = element_id("model_input")
+
+var model = null
+
+
+const load_model = () => {
+    let file = model_input.files[0]
+    let m_json = ""
+
+    const fr = new FileReader()
+    fr.onloadend = async () => {
+        model = await tf.loadLayersModel("http://localhost/number_model.json")
+        model.summary()
+    }
+
+    fr.readAsText(file, encoding="utf8")
+}
+
+const model_test = () => {
+    model = tf.sequential()
+    model.add(tf.layers.dense({units:2, inputShape:[2]}))
+    model.add(tf.layers.dense({units:1, inputShape:[2], activation:"sigmoid"}))
+    model.compile({loss: 'binaryCrossentropy', optimizer: 'sgd', metrics:['accuracy']});
+
+    model.summary()
+    test()
+}
+
+model_input.addEventListener('change', () => load_model())
+
+const fit = () => {
+    const history = await model.fit(tf.ones([8, 10]),
+        tf.ones([8, 1]), {
+        batchSize: 4,
+        epochs: 3
+    });
+}
+
--- static/js/main.js
+++ static/js/main.js
@@ -1,8 +1,21 @@
 
-const fileinput = document.getElementById("fileinput")
-const dataset = Array()
+
+const fileinput = element_id("fileinput")
+const trainCount = element_id('train')
+const testCount = element_id('test')
+const dataset_result = element_id('divide_result')
+
+var colname = Array()
+var dataset = Array()
+
+var Trainset = null
+var Testset = null
+
 
 const Send = async () => {
+    table.innerHTML = ""
+    dataset = Array()
+
     const files = fileinput.files;
 
     [...files].map(async (file) => {
@@ -38,13 +51,19 @@ const parseCsvToArray = (data) => {
       var rowCells = allRows[singleRow].split(',');
       for (var rowCell = 0; rowCell < rowCells.length; rowCell++) {
           let x = rowCells[rowCell]
-          if(singleRow != 0) {
-              x = parseFloat(x)
+          if(singleRow == 0) {
+              colname.push(x)
           }
-          arr.push(x)
+          else{
+            x = parseFloat(x)
+            if(isNaN(x)){ x = rowCells[rowCell] }
+            arr.push(x)
+          }
       }
-      
-      dataset.push(arr)
+      if(singleRow != 0)
+      {
+        dataset.push(arr)
+      }
     }
 }
 
@@ -52,14 +71,13 @@ function dataSetToTable() {
     var table = '<table>';
     table += '<thead>';
     table += '<tr>';
-    dataset[0].forEach((x) => {
+    colname.forEach((x) => {
         table += '<th>'
         table += String(x)
         table += '</th>'
     })
     table += '</tr>'
-    //for (var singleRow = 0; singleRow < allRows.length; singleRow++) {
-    dataset.slice(1, 6).forEach(x => {
+    dataset.slice(0, 5).forEach(x => {
         table += '<tr>';
         x.forEach(y => {
             table += '<td>'
@@ -72,8 +90,45 @@ function dataSetToTable() {
     table += '</tr>';
     table += '</tbody>';
     table += '</table>';
-    document.getElementById('table').innerHTML += table;
+    element_id('table').innerHTML += table;
 }
 
+const divide_ratio = () => {
+    let v = parseInt(trainCount.value)
+    testCount.value = String(10 - v)
+}
 
+const divideDataSet = () => {
+    let v = parseInt(trainCount.value)
+    let ratio = parseInt(dataset.length * (v/10))
+    console.log(ratio)
+    Trainset = dataset.slice(1, ratio)
+    TestSet = dataset.slice(ratio, dataset.length)
+    element_id('divide_result').innerHTML = `${Trainset.length} Train samples \n ${TestSet.length} Test samples`
+}
+
+var t = Array()
+var ty = Array()
+
+function test()
+{
+    for(var i=0; i < 100; i++){
+        t.push(dataset[i].slice(1,3))
+    }
+
+    for(var i=0; i < 100; i++){
+        if(dataset[i][5] == 'setosa')
+        {
+            ty.push(1)
+        }else{
+            ty.push(0)
+        }
+    }
+}
+
+element_id('divide').addEventListener('click', () => divideDataSet())
 fileinput.addEventListener('change', () => Send())
+trainCount.addEventListener('change', () => divide_ratio())
+
+trainCount.value = "8"
+divide_ratio()
